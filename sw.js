2b06d943236a9164f2e8ed3197885f52685e2f68
revaja69-crypto <revaja69@gmail.com>
2026-04-10 18:19:06 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cashflow-umkm-v3';
+const CACHE_NAME = 'cashflow-umkm-v4';
 const ASSETS = [
     './',
     'index.html',
@@ -16,6 +16,20 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
